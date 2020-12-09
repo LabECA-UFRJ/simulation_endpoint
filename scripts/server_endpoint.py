665c6e7aa270686334d96f04ce4c94352285e09d
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2020-12-09 19:04:03 -0300

    Initial commit

---
+++ scripts/server_endpoint.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import rospy
+
+from tcp_endpoint.RosTCPServer import TCPServer
+from tcp_endpoint.RosPublisher import RosPublisher
+from tcp_endpoint.RosSubscriber import RosSubscriber
+
+
+from std_msgs.msg import Empty
+from simulation_msgs.msg import Robot, PoseRobot, TwistRobot
+
+
+def main():
+    buffer_size = rospy.get_param("~buffer_size", 1024)
+    max_connections = rospy.get_param("~max_connections", 10)
+    tcp_server = TCPServer("UnityBridge", buffer_size, max_connections)
+
+    tcp_server.source_destination_dict = {
+        'presence': RosPublisher('presence', Empty, queue_size=10, latch=True),
+        # 'pose': RosPublisher('pos_rot', PosRot, queue_size=10),
+        'robot_def': RosSubscriber('robot_def', Robot, tcp_server),
+        'robot_cmd_vel': RosSubscriber('robot_cmd_vel', TwistRobot, tcp_server)
+    }
+
+    rospy.init_node("UnityBridge")
+    tcp_server.start()
+    rospy.spin()
+
+
+if __name__ == "__main__":
+    main()
